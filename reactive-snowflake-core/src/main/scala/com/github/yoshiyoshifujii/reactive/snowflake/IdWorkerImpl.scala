0820bf4ad645748bb2872b199fdfdc9c1600d983
Yoshitaka Fujii <yoshiyoshifujii@gmail.com>
2021-08-05 15:45:18 +0900

    implement Id worker

--- reactive-snowflake-core/src/main/scala/com/github/yoshiyoshifujii/reactive/snowflake/IdWorkerImpl.scala
+++ reactive-snowflake-core/src/main/scala/com/github/yoshiyoshifujii/reactive/snowflake/IdWorkerImpl.scala
@@ -1,15 +1,66 @@
 package com.github.yoshiyoshifujii.reactive.snowflake
 
-case class NextId(id: Option[Long], lastTimestamp: Long, nextSequence: Long)
+@SerialVersionUID(1L)
+final case class InvalidSystemClock(message: String, lastTimestamp: Long, lastSequenceId: Long)
+    extends Exception(message)
+
+final case class NextId(id: Option[Long], lastTimestamp: Long, nextSequence: Long)
+
+final case class Bits(private val value: Long) {
+  def maxId: Long          = -1L ^ (-1L << value)
+  def +(other: Bits): Bits = Bits(value + other.value)
+  def toLong: Long         = value
+}
 
 private[snowflake] trait IdWorkerImpl {
 
   protected val datacenterId: Long
   protected val workerId: Long
 
+  // 2010-11-04T01:42:54.657Z
+  // from https://github.com/twitter-archive/snowflake/blob/snowflake-2010/src/main/scala/com/twitter/service/snowflake/IdWorker.scala#L25
+  protected val twepoch                = 1288834974657L
+  protected val workerIdBits: Bits     = Bits(5L)
+  protected val datacenterIdBits: Bits = Bits(5L)
+  protected val sequenceBits: Bits     = Bits(12L)
+
+  private val maxWorkerId     = workerIdBits.maxId
+  private val maxDatacenterId = datacenterIdBits.maxId
+  private val maxSequenceId   = sequenceBits.maxId
+
+  private val workerIdShift      = sequenceBits.toLong
+  private val datacenterIdShift  = (sequenceBits + workerIdBits).toLong
+  private val timestampLeftShift = (sequenceBits + workerIdBits + datacenterIdBits).toLong
+
+  private val sequenceMask = sequenceBits.maxId
+
   def nextId(timestamp: Long, lastTimestamp: Long, currentSequence: Long): NextId = {
-    val id = Some(1L)
-    NextId(id, timestamp, 0L)
+
+    if (timestamp < lastTimestamp) {
+      throw InvalidSystemClock(
+        s"Clock moved backwards.  Refusing to generate id for ${lastTimestamp - timestamp} milliseconds",
+        lastTimestamp,
+        currentSequence
+      )
+    }
+
+    require(currentSequence < sequenceMask + 1)
+
+    val sequence = if (lastTimestamp == timestamp) {
+      val s = (currentSequence + 1) & sequenceMask
+      if (s == 0) None else Some(s)
+    } else {
+      Some(0L)
+    }
+
+    val id = sequence.map { s =>
+      ((timestamp - twepoch) << timestampLeftShift) |
+      (datacenterId << datacenterIdShift) |
+      (workerId << workerIdShift) |
+      s
+    }
+
+    NextId(id, timestamp, sequence.getOrElse(maxSequenceId))
   }
 
 }
--- reactive-snowflake-core/src/test/scala/com/github/yoshiyoshifujii/reactive/snowflake/IdWorkerImplSpec.scala
+++ reactive-snowflake-core/src/test/scala/com/github/yoshiyoshifujii/reactive/snowflake/IdWorkerImplSpec.scala
@@ -5,18 +5,130 @@ import org.scalatest.freespec.AnyFreeSpec
 class IdWorkerImplSpec extends AnyFreeSpec {
 
   "IdWWorkerImpl" - {
+    val workerMask     = 0x000000000001f000L
+    val datacenterMask = 0x00000000003e0000L
+    val timestampMask  = 0xffffffffffc00000L
 
     "generate an id" in {
       val worker = new IdWorkerImpl {
-        override protected val datacenterId: Long = 1L
-        override protected val workerId: Long = 1L
+        override val datacenterId: Long = 1L
+        override val workerId: Long     = 1L
       }
 
-      val timestamp = System.currentTimeMillis()
+      val timestamp              = System.currentTimeMillis()
       val NextId(Some(id), _, _) = worker.nextId(timestamp, timestamp, 0L)
       assert(id > 0L)
     }
 
+    "properly mask worker id" in {
+      val wId = 0x1fL
+
+      val worker = new IdWorkerImpl {
+        override val datacenterId: Long = 1L
+        override val workerId: Long     = wId
+      }
+      (1 to 1000).foreach { _ =>
+        val lastTimestamp = System.currentTimeMillis()
+        val id            = worker.nextId(System.currentTimeMillis(), lastTimestamp, 0L)
+        assert(((id.id.get & workerMask) >> 12) === wId)
+      }
+    }
+
+    "properly mask dc id" in {
+      val dcId = 0x1f
+      val wId  = 0
+
+      val worker = new IdWorkerImpl {
+        override val datacenterId: Long = dcId
+        override val workerId: Long     = wId
+      }
+
+      val lastTimestamp = System.currentTimeMillis()
+      val id            = worker.nextId(System.currentTimeMillis(), lastTimestamp, 0L)
+      assert(((id.id.get & datacenterMask) >> 17) === dcId)
+    }
+
+    "properly mask timestamp" in {
+      val epo = 1288834974657L
+      val worker = new IdWorkerImpl {
+        override protected val datacenterId: Long = 1L
+        override protected val workerId: Long     = 1L
+        override protected val twepoch: Long      = epo
+      }
+      (1 to 100).foreach { _ =>
+        val t  = System.currentTimeMillis()
+        val id = worker.nextId(t, t - 1L, 0L)
+        ((id.id.get & timestampMask) >> 22) === (t - epo)
+      }
+    }
+
+    "roll over sequence id" in {
+      val dcId = 4
+      val wId  = 4
+      val worker = new IdWorkerImpl {
+        override protected val datacenterId: Long = dcId
+        override protected val workerId: Long     = wId
+      }
+      var sequence: Long = 0x000
+      val endSequence    = 0xfff
+
+      val timestamp = System.currentTimeMillis()
+
+      (sequence until endSequence).foreach { _ =>
+        val id = worker.nextId(timestamp, timestamp, sequence)
+        assert(((id.id.get & workerMask) >> 12) === wId)
+        sequence = id.nextSequence
+      }
+    }
+
+    "does not generate ids over max sequence" in {
+      val dcId = 4
+      val wId  = 4
+      val worker = new IdWorkerImpl {
+        override protected val datacenterId: Long = dcId
+        override protected val workerId: Long     = wId
+      }
+
+      val timestamp = System.currentTimeMillis()
+
+      val id = worker.nextId(timestamp, timestamp, 0xfff)
+      assert(id.id === None)
+    }
+
+    "generate increasing ids" in {
+      val worker = new IdWorkerImpl {
+        override protected val datacenterId: Long = 1L
+        override protected val workerId: Long     = 1L
+      }
+
+      var lastId          = 0L
+      var currentSequence = 0L
+
+      (1 to 100).foreach { _ =>
+        val lastTimestamp = System.currentTimeMillis()
+        val id            = worker.nextId(System.currentTimeMillis(), lastTimestamp, currentSequence)
+        assert(id.id.get > lastId)
+        lastId = id.id.get
+        currentSequence = id.nextSequence
+      }
+    }
+
+    "generate 1 million ids quickly" in {
+      val worker = new IdWorkerImpl {
+        override protected val datacenterId: Long = 1L
+        override protected val workerId: Long     = 1L
+      }
+      var lastTimestamp = -1L
+      val t             = System.currentTimeMillis()
+      (1 to 1_000_000).foreach { _ =>
+        val currentTimestamp = System.currentTimeMillis()
+        worker.nextId(currentTimestamp, lastTimestamp, 0L)
+        lastTimestamp = currentTimestamp
+      }
+      val t2 = System.currentTimeMillis()
+      println("generated 1,000,000 ids in %d ms, or %,.0f ids/second".format(t2 - t, 1000000000.0 / (t2 - t)))
+      assert(1 === 1)
+    }
   }
 
 }
